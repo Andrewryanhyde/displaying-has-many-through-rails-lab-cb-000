4d599eedd897ccd6b194607ee481433b60ee0393
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-08-28 13:35:20 +0000

    automatically backed up by learn

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,6 +1,10 @@
 class DoctorsController < ApplicationController
 
-  def index 
-    @doctors = Doctor.all 
+  def index
+    @doctors = Doctor.all
+  end
+
+  def show 
+    @doctor = Doctor.find_by(params[:id])
   end
 end
